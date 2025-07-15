6e83914a9b13eb46a4fa66803a8d35af93c4e649
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2025-07-15 22:57:32 +0200

    Update improved_fps_booster_load.lua

--- improved_fps_booster/lua/autorun/improved_fps_booster_load.lua
+++ improved_fps_booster/lua/autorun/improved_fps_booster_load.lua
@@ -44,26 +44,22 @@ else
     local Ipr_Resource = {"resource/fonts/Rajdhani-Bold.ttf", "materials/icon/ipr_boost_computer.png", "materials/icon/ipr_boost_wrench.png"}
     for i = 1, #Ipr_Resource do
         local Ipr_Resources = Ipr_Resource[i]
-
         resource.AddFile(Ipr_Resources)
     end
 
     for i = 1, #Ipr_FilesConfig do
         local Ipr_Config = Ipr_FilesConfig[i]
-
         AddCSLuaFile("ipr_fps_booster_configuration/" ..Ipr_Config)
     end
     
     for i = 1, #Ipr_FilesLang do
         local Ipr_Lang = Ipr_FilesLang[i]
-
         AddCSLuaFile("ipr_fps_booster_language/" ..Ipr_Lang)
     end
     
     local Ipr_FilesClient = file.Find("ipr_fps_booster_lua/client/*", "LUA")
     for i = 1, #Ipr_FilesClient do
         local Ipr_FClient = Ipr_FilesClient[i]
-
         AddCSLuaFile("ipr_fps_booster_lua/client/" ..Ipr_FClient)
     end
     
