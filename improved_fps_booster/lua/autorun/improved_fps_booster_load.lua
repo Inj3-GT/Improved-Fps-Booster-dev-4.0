1747ef55447983daeebc91b74958267a716d420a
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2025-07-15 06:28:41 +0200

    Add files via upload

--- improved_fps_booster/lua/autorun/improved_fps_booster_load.lua
+++ improved_fps_booster/lua/autorun/improved_fps_booster_load.lua
@@ -6,15 +6,15 @@
 
 Ipr_Fps_Booster = Ipr_Fps_Booster or {}
 
-local Ipr_SendFileCLConfig = file.Find("ipr_fps_booster_configuration/*", "LUA")
-local Ipr_SendFileLangCL = file.Find("ipr_fps_booster_language/*", "LUA")
+local Ipr_FilesConfig = file.Find("ipr_fps_booster_configuration/*", "LUA")
+local Ipr_FilesLang = file.Find("ipr_fps_booster_language/*", "LUA")
 
 Ipr_Fps_Booster.Settings = Ipr_Fps_Booster.Settings or {}
 Ipr_Fps_Booster.Settings.Version = "4.0"
+Ipr_Fps_Booster.Settings.Developer = "Inj3"
 
 if (CLIENT) then
     Ipr_Fps_Booster.Settings.DefaultLanguage = "EN"
-    Ipr_Fps_Booster.Settings.Developer = "Inj3"
     
     surface.CreateFont("Ipr_Fps_Booster_Font",{
         font = "Rajdhani Bold",
@@ -26,34 +26,45 @@ if (CLIENT) then
     Ipr_Fps_Booster.Convars = Ipr_Fps_Booster.Convars or {}
     Ipr_Fps_Booster.Lang = {}
 
-    for _, f in pairs(Ipr_SendFileLangCL) do
-       local Ipr_RefLang = string.upper(string.gsub(f, ".lua", ""))
-       Ipr_Fps_Booster.Lang[Ipr_RefLang] = include("ipr_fps_booster_language/"..f)
-    end
-    
-    for _, f in pairs(Ipr_SendFileCLConfig) do
-        local Ipr_RefConfig = "Default" ..string.lower(string.gsub(f, ".lua", ""))
-        Ipr_Fps_Booster[Ipr_RefConfig] = include("ipr_fps_booster_configuration/"..f)
+    for i = 1, #Ipr_FilesLang do
+        local Ipr_Lang = Ipr_FilesLang[i]
+        local Ipr_RefLang = string.upper(string.gsub(Ipr_Lang, ".lua", ""))
+
+        Ipr_Fps_Booster.Lang[Ipr_RefLang] = include("ipr_fps_booster_language/"..Ipr_Lang)
     end
 
+    for i = 1, #Ipr_FilesConfig do
+        local Ipr_Config = Ipr_FilesConfig[i]
+        local Ipr_RefConfig = "Default" ..string.lower(string.gsub(Ipr_Config, ".lua", ""))
+        
+        Ipr_Fps_Booster[Ipr_RefConfig] = include("ipr_fps_booster_configuration/"..Ipr_Config)
+    end
+    
     include("ipr_fps_booster_lua/client/vgui.lua")
 else
     local Ipr_Resource = {"resource/fonts/Rajdhani-Bold.ttf", "materials/icon/ipr_boost_computer.png", "materials/icon/ipr_boost_wrench.png"}
-    for _, v in pairs(Ipr_Resource) do
-        resource.AddFile(v)
+    for i = 1, #Ipr_Resource do
+        local Ipr_Resources = Ipr_Resource[i]
+
+        resource.AddFile(Ipr_Resources)
     end
 
-    for _, f in pairs(Ipr_SendFileLangCL) do
-        AddCSLuaFile("ipr_fps_booster_language/"..f)
+    for i = 1, #Ipr_FilesConfig do
+        local Ipr_Config = Ipr_FilesConfig[i]
+
+        AddCSLuaFile("ipr_fps_booster_configuration/" ..Ipr_Config)
     end
-    for _, f in pairs(Ipr_SendFileCLConfig) do
-        AddCSLuaFile("ipr_fps_booster_configuration/"..f)
+    for i = 1, #Ipr_FilesLang do
+        local Ipr_Lang = Ipr_FilesLang[i]
+
+        AddCSLuaFile("ipr_fps_booster_language/" ..Ipr_Lang)
+    end
+    local Ipr_FilesClient = file.Find("ipr_fps_booster_lua/client/*", "LUA")
+    for i = 1, #Ipr_FilesClient do
+        local Ipr_FClient = Ipr_FilesClient[i]
+
+        AddCSLuaFile("ipr_fps_booster_lua/client/" ..Ipr_FClient)
     end
     
-    local Ipr_SendFileCL = file.Find("ipr_fps_booster_lua/client/*", "LUA")
-    for _, f in pairs(Ipr_SendFileCL) do
-        AddCSLuaFile("ipr_fps_booster_lua/client/"..f)
-    end
-    
-    MsgC(Color(0, 250, 0), "\nImproved FPS Booster System " ..Ipr_Fps_Booster.Settings.Version.. " by Inj3\n")
+    MsgC(Color(0, 250, 0), "\nImproved FPS Booster System " ..Ipr_Fps_Booster.Settings.Version.. " by " ..Ipr_Fps_Booster.Settings.Developer.. "\n")
 end
