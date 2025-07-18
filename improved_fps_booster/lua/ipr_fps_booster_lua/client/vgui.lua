66cf72ff397771f64f0132d19bac4a02d920b655
Inj3 <58576616+Inj3-GT@users.noreply.github.com>
2025-07-18 06:47:22 +0200

    Update vgui.lua

--- improved_fps_booster/lua/ipr_fps_booster_lua/client/vgui.lua
+++ improved_fps_booster/lua/ipr_fps_booster_lua/client/vgui.lua
@@ -612,7 +612,7 @@ local function Ipr_FpsBooster()
 
     local Ipr_PResetFps = vgui.Create("DButton", Ipr.Settings.Vgui.Primary)
     Ipr_PResetFps:SetSize(150, 21)
-    Ipr_PResetFps:SetPos(Ipr_PSize.w / 2 - Ipr_PResetFps:GetWide() / 2 + 1, 191)
+    Ipr_PResetFps:SetPos(Ipr_PSize.w / 2 - Ipr_PResetFps:GetWide() / 2 + 2, 191)
     Ipr_PResetFps:SetText("")
     Ipr.Function.SetToolTip(Ipr_Fps_Booster.Lang[Ipr.Settings.SetLang].TReset, Ipr_PResetFps, true)
     Ipr_PResetFps.Paint = function(self, w, h)
